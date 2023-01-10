8c9f312f3d920afcaee708b34ea7bbfadbd806a3
jiannei <longjian.huang@foxmail.com>
2023-01-10 17:15:34 +0800

    feat: rules

--- src/Crawler.php
+++ src/Crawler.php
@@ -19,6 +19,26 @@ class Crawler extends SymfonyCrawler
         return $this->new($html);
     }
 
+    public function rules(array $rules):array
+    {
+        return $this->each(function (SymfonyCrawler $node) use ($rules) {
+            $item = [];
+            foreach ($rules as $field => $rule) {
+                // Todo more check
+                $selector = $rule[0];
+                $method = $rule[1];
+
+                if (!$node->filter($selector)->count()) {
+                    continue;
+                }
+
+                $item[$field] = !in_array($method, ['text', 'html']) ? $node->filter($selector)->attr($method) : $node->filter($selector)->$method();
+            }
+
+            return $item;
+        });
+    }
+
     public function attrs(string $attribute):array
     {
         return $this->each(function (SymfonyCrawler $node) use ($attribute){
--- tests/FetchTest.php
+++ tests/FetchTest.php
@@ -14,7 +14,57 @@ class FetchTest extends TestCase
         $author = $crawler->filter('#author_baidu>strong')->text();
         $content = $crawler->filter('.post_content')->html();
 
-        $this->assertEquals('巴基斯坦一城镇温度达50.2度：创下全球4月历史温度新高',$title);
-        $this->assertEquals('白猫',$author);
+        $this->assertEquals('巴基斯坦一城镇温度达50.2度：创下全球4月历史温度新高', $title);
+        $this->assertEquals('白猫', $author);
+    }
+
+    public function testRules()
+    {
+        $crawler = Crawler::fetch('https://www.ithome.com/html/discovery/358585.htm');
+
+        $rules = [
+            'title' => ['h1', 'text'],
+            'author' => ['#author_baidu>strong', 'text'],
+            'content' => ['.post_content','html']
+        ];
+
+        // 解析文章详情
+        $article = [
+            'title' => $crawler->filter('h1')->text(),
+            'author' => $crawler->filter('#author_baidu>strong')->text(),
+            'content' => $crawler->filter('.post_content')->html(),
+        ];
+
+        // 等价于
+        $article2 = $crawler->rules($rules);
+
+        $this->assertEquals($article, $article2[0]);
+    }
+
+    public function testRulesAdvanced()
+    {
+        $crawler = Crawler::fetch('https://it.ithome.com/ityejie');
+
+        $rules = [
+            'title' => ['h2>a', 'text'],
+            'link' => ['h2>a', 'href'],
+            'img' => ['a>img', 'src'],
+            'desc' => ['.m', 'text'],
+        ];
+
+        // 解析文章列表
+        $articles = $crawler->filter('.bl li')->each(function ($node) use ($rules) {
+            return [
+                'title' => $node->filter('h2>a')->text(),
+                'link' => $node->filter('h2>a')->attr('href'),
+                'img' => $node->filter('a img')->attr('src'),
+                'desc' => $node->filter('.m')->text(),
+            ];
+        });
+
+        // 等价于
+        $articles2 = $crawler->filter('.bl li')->rules($rules);
+
+        $this->assertEquals($articles,$articles2);
     }
 }
